bb71b297f9cd3f340efd5907c5f1e13aa560de5e
Houruizhi <43208624+Houruizhi@users.noreply.github.com>
2018-11-07 19:59:27 +0800

    Add Python code for example 3-1

---
+++ DIP3E/Codes/Python/Gonzalez_example_3_1.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# Gozalez examples 3.1 
+# contrast enhance using gamma transformation 
+
+# In[1]:
+
+
+import matplotlib.image as mpimg 
+import matplotlib.pyplot as plt
+
+# Read Images 
+img = mpimg.imread('Fig0308(a)(fractured_spine).tif') 
+  
+# Output Images 
+plt.imshow(img,'gray')
+
+
+# In[2]:
+
+
+img_b = img**0.6
+img_c = img**0.4
+img_d = img**0.3
+
+
+# In[3]:
+
+
+plt.figure()
+plt.subplot(1,3,1)
+plt.imshow(img_b,'gray')
+plt.subplot(1,3,2)
+plt.imshow(img_c,'gray')
+plt.subplot(1,3,3)
+plt.imshow(img_c,'gray')
+
