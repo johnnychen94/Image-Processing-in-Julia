6d03be01b257fe38c8ad14080738de476d771c54
JohnnyChen <johnnychen94@hotmail.com>
2019-02-10 15:36:51 +0800

    Reformatted example 3.1 and 3.2

--- DIP3E/Codes/Python/Chapter-3/Tracking/Example3-1.py
+++ DIP3E/Codes/Python/Chapter-3/Tracking/Example3-1.py
@@ -1,17 +1,20 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# Gozalez examples 3.1 
-# contrast enhance using gamma transformation 
+# **Examples 3.1:** Contrast enhance using gamma transformation
+# 
+# This example includes figure 3.8.
+# 
+# > This code is written with python==3.7, skimage==0.14, matplotlib==2.2.3
 
 # In[1]:
 
 
-import matplotlib.image as mpimg 
+from skimage.io import imread
 import matplotlib.pyplot as plt
 
 # Read Images 
-img = mpimg.imread('./Data/Fig0308(a)(fractured_spine).tif') 
+img = imread('./Data/Fig0308(a)(fractured_spine).tif').astype(float)
   
 # Output Images 
 plt.imshow(img,'gray')
@@ -20,6 +23,7 @@ plt.imshow(img,'gray')
 # In[2]:
 
 
+# contrast manipulation by power-law transformation
 img_b = img**0.6
 img_c = img**0.4
 img_d = img**0.3
@@ -28,11 +32,14 @@ img_d = img**0.3
 # In[3]:
 
 
-plt.figure()
-plt.subplot(1,3,1)
+# Figure 3.8
+plt.figure(figsize=(15,15))
+plt.subplot(2,2,1)
 plt.imshow(img_b,'gray')
-plt.subplot(1,3,2)
+plt.subplot(2,2,2)
+plt.imshow(img_b,'gray')
+plt.subplot(2,2,3)
 plt.imshow(img_c,'gray')
-plt.subplot(1,3,3)
+plt.subplot(2,2,4)
 plt.imshow(img_c,'gray')
 
--- DIP3E/Codes/Python/Chapter-3/Tracking/Example3-2.py
+++ DIP3E/Codes/Python/Chapter-3/Tracking/Example3-2.py
@@ -1,18 +1,21 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# Gozalez examples 3.2
-# gamma transformation 2
+# **Examples 3.2:** Another illustration of power-law transformations.
+# 
+# This example includes figure 3.9.
+# 
+# > This code is written with python==3.7, skimage==0.14, matplotlib==2.2.3
 
 # In[1]:
 
 
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-from skimage import data,img_as_float
+from skimage.io import imread
 
 # Read Images 
-img = mpimg.imread('Fig0309(a)(washed_out_aerial_image).tif') 
+img = imread('./Data/Fig0309(a)(washed_out_aerial_image).tif').astype(float)
+
 # Output Images 
 plt.imshow(img,'gray')
 
@@ -20,18 +23,24 @@ plt.imshow(img,'gray')
 # In[2]:
 
 
-img_b = img_as_float(img)**3
-img_c = img_as_float(img)**4
-img_d = img_as_float(img)**5
+# contrast manipulation by power-law transformation
+img_b = img**3.0
+img_c = img**4.0
+img_d = img**5.0
 
 
 # In[3]:
 
 
-plt.figure()
+# Figure 3.9
+plt.figure(figsize=(15,15))
+plt.subplot(2,2,1)
 plt.imshow(img_b,'gray')
-plt.figure()
+plt.subplot(2,2,2)
+plt.imshow(img_b,'gray')
+plt.subplot(2,2,3)
 plt.imshow(img_c,'gray')
-plt.figure()
-plt.imshow(img_d,'gray')
+plt.subplot(2,2,4)
+plt.imshow(img_c,'gray')
+plt.show()
 
